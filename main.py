3b8857a7e7545b57e171077fd88b0f878674fc05
benleong0 <benleong@urbanjungle.com>
2021-11-16 09:17:45 +0000

    combine imports

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select, WebDriverWait
 
 os.environ['PATH'] += r";C:/Users/benle/SeleniumDrivers"
 
